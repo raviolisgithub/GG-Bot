729ead1ce1d30808d9b138bce33cb1ea721aa47a
raviolisgithub <aaravrajup@gmail.com>
2022-10-05 20:43:01 +0400

    lol

--- index.js
+++
@@ -1,9 +0,0 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: GatewayIntentBits.Guilds });
-const { token } = require("./config.json");
-
-client.once("ready", () => {
-    console.log("Oh, hey!");
-});
-
-client.login(token);
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int int1, int2;
+    printf("Number 1: \n");
+    scanf("%d", &int1);
+    printf("Number 2: \n");
+    scanf
+}
