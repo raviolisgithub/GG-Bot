e793937c2d1974715b7580e79155065ba63937bb
raviolisgithub <aaravrajup@gmail.com>
2022-10-05 20:45:39 +0400

    amazed

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 
 int main() {
     int int1, int2;
-    printf("Number 1: \n");
+    printf("Number 1: ");
     scanf("%d", &int1);
-    printf("Number 2: \n");
-    scanf
+    printf("Number 2: ");
+    scanf("%d", &int2);
+    printf("Answer is %d", int1 + int2);
 }
